e491ff39e9541b17bf4bd75dbc4b07d13db383fa
pedroDeAlba123 <pedrodealbaalegre@gmail.com>
2022-07-05 14:43:43 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import { Redirect, Route, Switch } from "react-router-dom";
+import React, { Suspense } from "react";
+
+import Layout from "./components/layout/Layout";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+
+const NewQuotes = React.lazy(() => import("./pages/NewQuotes"));
+const AllQuotes = React.lazy(() => import("./pages/AllQuotes"));
+const QuoteDetail = React.lazy(() => import("./pages/QuoteDetail"));
+const NotFound = React.lazy(() => import("./pages/NotFound"));
+
+function App() {
+  return (
+    <Layout>
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/quotes" />
+          </Route>
+          <Route path="/quotes" exact>
+            <AllQuotes />
+          </Route>
+          <Route path="/quotes/:quoteId">
+            <QuoteDetail />
+          </Route>
+          <Route path="/new-quote">
+            <NewQuotes />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Suspense>
+    </Layout>
+  );
+}
+
+export default App;
---
+++ src/pages/QuoteDetail.js
@@ -0,0 +1,60 @@
+import { Fragment, useEffect } from "react";
+import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
+
+import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+import { getSingleQuote } from "../lib/api";
+import useHttp from "../hooks/use-http";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+
+const QuoteDetail = (props) => {
+  const match = useRouteMatch();
+  const params = useParams();
+
+  const { quoteId } = params;
+
+  const {
+    sendRequest,
+    status,
+    data: loadedQuote,
+    error,
+  } = useHttp(getSingleQuote, true);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  if(status === 'pending') {
+    return (
+      <div className="centered">
+        <LoadingSpinner/>
+      </div>
+    );
+  }
+
+  if(error) {
+    return <p className="centered">{error}</p>
+  }
+
+  if(!loadedQuote.text) {
+    return <p>No quote found!</p>;
+  }
+
+  return (
+    <Fragment>
+      <HighlightedQuote author={loadedQuote.author} text={loadedQuote.text} />
+      <Route path={match.path} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`${match.url}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+      <Route path={`${match.path}/comments`}>
+        <Comments />
+      </Route>
+    </Fragment>
+  );
+};
+
+export default QuoteDetail;
